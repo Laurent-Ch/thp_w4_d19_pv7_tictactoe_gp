cffdd2e3fb351d2fc186a94cbe4e97433edcdfdc
mrprst <prost.mathias@gmail.com>
2021-10-21 14:31:36 +0200

    game methods and delete case.rb

--- lib/game.rb
+++ lib/game.rb
@@ -23,5 +23,26 @@ end
     (@array_ttt.has_got_a_winner? || @array_ttt.is_over_without_winner?) false : true
   end
 
-
+  def playing_choice
+    puts "Choisissez une case, A/B/C pour la ligne et 1/2/3 pour la colonne. Par exemple A1"
+    choice = gets.chomp.upcase
+    possible_moves = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
+    until (possible_moves).include?(choice) do
+        puts "je n'ai pas compris, rentrez la coordonnée exacte"
+        choice = gets.chomp.upcase
+    end
+    return choice
+  end
+  
+  def playing_move(playing_choice, array, player)
+    array = [0,0,0,0,0,0,0,0,0]
+    possible_moves = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
+    possible_moves.each do |i|
+    if i.include?(playing_choice) 
+      array.insert(possible_moves.index(i),"X")
+    end
+  end
+      return array 
+  end
+  
   end
