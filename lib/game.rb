955c5c752822d851b446ac193035b62d6cdf813e
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-10-21 14:36:22 +0200

    adding a play_another_game? method

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,27 @@
 # require 'pry'
 class Game
- attr_accessor :player1, :player2, :starting_player, :following_player, :array_ttt
+ attr_accessor :player1, :player2, :starting_player, :following_player
 
- def initialize(name1,name2)
-  @player1 = Player.new(name1)
-  @player2 = Player.new(name2)
-  puts "Bienvenue à #{name1} et #{name2}!"
-end
-
-  def defining_starting_player
-    coin_toss = rand(1..2)
-    if coin_toss == 1
-      @starting_player = @player1
-      @following_player = @player2
-    else
-      @starting_player = @player2
-      @following_player = @player1
+  def initialize(name1,name2)
+    @player1 = Player.new(name1)
+    @player2 = Player.new(name2)
+    puts "Bienvenue à #{name1} et #{name2}!"
+  end
+  
+    def defining_starting_player
+      coin_toss = rand(1..2)
+      if coin_toss == 1
+        @starting_player = @player1
+        @following_player = @player2
+      else
+        @starting_player = @player2
+        @following_player = @player1
+      end
     end
-  end
 
-  def is_still_ongoing?
-    (@array_ttt.has_got_a_winner? || @array_ttt.is_over_without_winner?) false : true
-  end
+    # [...]
 
+<<<<<<< Updated upstream
   def playing_choice
     puts "Choisissez une case, A/B/C pour la ligne et 1/2/3 pour la colonne. Par exemple A1"
     choice = gets.chomp.upcase
@@ -46,3 +45,26 @@ end
   end
   
   end
+=======
+    def play_another_game?
+      puts "this game is over. Do you want to play another round (y/n)?"
+      answer = gets.chomp
+      until answer == "y" || answer == "n"
+        puts "I didn't get your answer, please type it again."
+        answer = gets.chomp
+      end
+      case
+      when answer == "y"
+        puts "link this with the start of another round"
+      when answer == "n"
+        puts "OK ! Thanks for playing"
+        exit!
+      end
+        
+    # def still_ongoing ?
+    #   # Pseudocode victory? == false && draw == false ? true : false
+    # end
+
+  end
+end
+>>>>>>> Stashed changes
